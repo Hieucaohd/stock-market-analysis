fb56681137e1535adb005dc7f4871568d2e4ed6a
hieucaohd <hieucaohd@gmail.com>
2024-02-06 07:16:04 +0000

    oke

--- build-infra/scripts/install.sh
+++ build-infra/scripts/install.sh
@@ -6,8 +6,20 @@ setEnv() {
 
 prepareEnvFile() {
 	if [ ! -f .env ]; then
+		echo "File .env does not exists."
 		cp build-infra/.env.tmpl .env
 		echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" >> .env
+	else
+		echo "File .env already exists."
+	fi
+}
+
+prepareDockerCompose() {
+	if [ ! -f docker-compose.yml ]; then
+		echo "File docker-compose.yml does not exists."
+		cp build-infra/docker-compose.tmpl.yml docker-compose.yml
+	else
+		echo "File docker-compose.yml already exists."
 	fi
 }
 
@@ -18,21 +30,21 @@ prepareConfig() {
 	spark_config="spark"
 
 	if [ ! -d "$config_folder/$hadoop_config" ]; then
-		echo "Folder $config_folder/$hadoop_config does not exist."
+		echo "Folder $config_folder/$hadoop_config does not exists."
 		cp -r "$config_folder/template/$hadoop_config" "$config_folder/$hadoop_config"
 	else
 		echo "Folder $config_folder/$hadoop_config already exists."
 	fi
 
 	if [ ! -d "$config_folder/$hive_config" ]; then
-		echo "Folder $config_folder/$hive_config does not exist."
+		echo "Folder $config_folder/$hive_config does not exists."
 		cp -r "$config_folder/template/$hive_config" "$config_folder/$hive_config"
 	else
 		echo "Folder $config_folder/$hive_config already exists."
 	fi
 
 	if [ ! -d "$config_folder/$spark_config" ]; then
-		echo "Folder $config_folder/$spark_config does not exist."
+		echo "Folder $config_folder/$spark_config does not exists."
 		cp -r "$config_folder/template/$spark_config" "$config_folder/$spark_config"
 	else
 		echo "Folder $config_folder/$spark_config already exists."
@@ -46,6 +58,7 @@ main() {
 	prepareEnvFile
 	setEnv
 	prepareConfig
+	prepareDockerCompose
 }
 
 main "$@"
