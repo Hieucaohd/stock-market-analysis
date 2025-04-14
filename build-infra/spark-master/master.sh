9675bc8ef053320269f7b79c3b42a461d0559dc9
hieucaohd <hieucaohd@gmail.com>
2025-04-14 17:16:44 +0000

    ok

--- build-infra/spark-master/master.sh
+++ build-infra/spark-master/master.sh
@@ -9,5 +9,10 @@ mkdir -p $SPARK_MASTER_LOG
 
 ln -sf /dev/stdout $SPARK_MASTER_LOG/spark-master.out
 
-cd $SPARK_HOME/bin && $SPARK_HOME/sbin/../bin/spark-class org.apache.spark.deploy.master.Master \
-    --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG/spark-master.out
+# cd $SPARK_HOME/bin && $SPARK_HOME/sbin/../bin/spark-class org.apache.spark.deploy.master.Master \
+#     --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG/spark-master.out
+
+while true; do
+    echo "Looping forever..."
+    sleep 1  # Tạm dừng 1 giây để tránh quá tải CPU
+done
